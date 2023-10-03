a588ed1d8e86d97cce594e37179d0fe1e30d2a26
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-03 17:03:56 +0600

    Add lineThrough and do some minor change

--- lib/utility/todo_tile.dart
+++ lib/utility/todo_tile.dart
@@ -15,9 +15,13 @@ class ToDoTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+      padding: const EdgeInsets.only(
+        left: 25,
+        right: 25,
+        top: 15,
+      ),
       child: Container(
-        padding: const EdgeInsets.all(15),
+        padding: const EdgeInsets.all(8),
         decoration: BoxDecoration(
           color: const Color.fromRGBO(246, 252, 253, 1),
           borderRadius: BorderRadius.circular(15),
@@ -25,10 +29,21 @@ class ToDoTile extends StatelessWidget {
         child: Row(
           children: [
             // Checkbox
-            Checkbox(value: taskCompleted, onChanged: onChanged),
+            Checkbox(
+              value: taskCompleted,
+              onChanged: onChanged,
+              activeColor: const Color.fromRGBO(34, 34, 34, 0.875),
+            ),
 
             //Task
-            const Text('Task'),
+            Text(
+              'Task',
+              style: TextStyle(
+                decoration: taskCompleted
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+              ),
+            ),
           ],
         ),
       ),
