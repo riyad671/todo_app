2914cc4e581abc220ec68ef3c988aca0feab9981
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-03 16:52:50 +0600

    Add functionality in tap option

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,20 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/utility/todo_tile.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // List of To-Do task
+  List toDoList = [
+    [
+      "Finish To-Do App",
+      false,
+    ],
+    [
+      "Compile quiz app",
+      false,
+    ],
+  ];
+  // Checkbox was taped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(191, 232, 255, 1),
       appBar: AppBar(
         title: const Text('To Do'),
+        centerTitle: true,
+        titleTextStyle: const TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+        ),
+        // Icon
+        leading: IconButton(
+          icon: const Icon(Icons.menu), // Add your icon here
+          onPressed: () {
+            // Add your action when the icon is pressed
+          },
+        ),
         elevation: 0.9,
       ),
-      body: ListView(
-        children: const [
-          ToDoTile(),
-        ],
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
       ),
     );
   }
--- lib/utility/todo_tile.dart
+++ lib/utility/todo_tile.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
 
 class ToDoTile extends StatelessWidget {
-  const ToDoTile({super.key});
+  const ToDoTile({
+    super.key,
+    required this.taskCompleted,
+    required this.taskName,
+    required this.onChanged,
+  });
+
+  final String taskName;
+  final bool taskCompleted;
+  final Function(bool?)? onChanged;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(15),
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
       child: Container(
-        padding: const EdgeInsets.all(18),
+        padding: const EdgeInsets.all(15),
         decoration: BoxDecoration(
           color: const Color.fromRGBO(246, 252, 253, 1),
           borderRadius: BorderRadius.circular(15),
         ),
-        child: const Text('Data'),
+        child: Row(
+          children: [
+            // Checkbox
+            Checkbox(value: taskCompleted, onChanged: onChanged),
+
+            //Task
+            const Text('Task'),
+          ],
+        ),
       ),
     );
   }
